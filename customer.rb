fd10f596be819650e150623d026a9f4a0d20eff9
Rubén Gil <ruben@nuatt.es>
2013-06-07 13:43:56 +0200

    Refactoring 3: Move method amount_for to Rental

--- customer.rb
+++ customer.rb
@@ -14,16 +14,15 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
-      #add frequent renter points
+    #add frequent renter points
       frequent_renter_points +=1
       #add bonus for a two day new release rental
       if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1 
         frequent_renter_points += 1
       end
       #show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount = this_amount  
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount = element.charge  
     end
     #add footer lines
     result += "Amount owed is #{total_amount}\n"
@@ -31,19 +30,5 @@ class Customer
     result
   end
 
-  def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result +=2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-    result
-  end
 end
 
