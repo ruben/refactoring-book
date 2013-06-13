5ec46e3afee31de1270a529966af607ec0e0c59c
Rubén Gil <ruben@nuatt.es>
2013-06-13 13:21:44 +0200

    Refactoring 6: Extract temp to query (total_frequent_renter_points)

--- customer.rb
+++ customer.rb
@@ -11,21 +11,23 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0
     result = "Rental Record for #{name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
       #show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
     #add footer lines
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
 
