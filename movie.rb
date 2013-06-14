b74594aa40df9888f22d7f7de20d9df01b780ee4
Rubén Gil <ruben@nuatt.es>
2013-06-14 12:40:31 +0200

    Solved movie.rb indenting

--- movie.rb
+++ movie.rb
@@ -1,13 +1,13 @@
 class Movie
-	REGULAR = 0
-	NEW_RELEASE = 1
-	CHILDRENS = 2
+  REGULAR = 0
+  NEW_RELEASE = 1
+  CHILDRENS = 2
 
-	attr_reader :title
+  attr_reader :title
 
-	def initialize(title, price)
-		@title, @price = title, price
-	end
+  def initialize(title, price)
+    @title, @price = title, price
+  end
 
   def charge(days_rented)
     return @price.charge(days_rented)
